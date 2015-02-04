d8f27b2a64c11843d87efad29a5d0d0a8a295532
StellaEngle <stella.engle@hotmail.com>
2015-02-04 16:22:06 +0000

    added settings and products

--- app/src/main/java/com/ncl/team5/lloydsmockup/Settings.java
+++ app/src/main/java/com/ncl/team5/lloydsmockup/Settings.java
@@ -5,15 +5,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
-public class Settings extends Activity {
-
+public class Settings extends Activity implements AdapterView.OnItemSelectedListener{
+    private TextView changeFontSize;
+    private static final String[] fontSizes={"Size 12", "Size 14", "Size 16"};
+    private static final String[] appColours={"Blue", "Red","Yellow"};
+    private static final String[] timeInter={"10 Minutes","15 Minutes", "30 Minutes"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        changeFontSize=(TextView)findViewById(R.id.changeFontSize);
+
+        Spinner spin=(Spinner)findViewById(R.id.spinnerFont);
+        Spinner spinCol=(Spinner)findViewById(R.id.spinnerColour);
+        Spinner spinTime=(Spinner)findViewById(R.id.spinnerTime);
+
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter<String> cFontSize=new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                fontSizes);
+        ArrayAdapter<String> colour= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,appColours);
+        ArrayAdapter<String> timer=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,timeInter);
+        cFontSize.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        colour.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+        timer.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinCol.setAdapter(colour);
+        spin.setAdapter(cFontSize);
+        spinTime.setAdapter(timer);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent,
+                               View v, int position, long id) {
+        //selection.setText(items[position]);
+        if(fontSizes[position].compareTo("Size 12")==0){
+            changeFontSize.setTextSize(12);
+        }else if(fontSizes[position].compareTo("Size 14")==0){
+            changeFontSize.setTextSize(14);
+        }else{
+            changeFontSize.setTextSize(16);
+        }
+
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        changeFontSize.setText("");
+    }
+
 
 
     @Override
