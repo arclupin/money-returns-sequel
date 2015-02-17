937aaa2b74eacea446e62de7eeefa2afc6505957
hairygiraffe <b.t.lambert@ncl.ac.uk>
2015-02-17 18:36:51 +0000

    added cookies

--- app/src/main/java/HTTPConnect/Connection.java
+++ app/src/main/java/HTTPConnect/Connection.java
@@ -12,6 +12,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 
+
 import com.ncl.team5.lloydsmockup.Login;
 
 import java.io.BufferedReader;
@@ -29,12 +30,18 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.json.JSONObject;
+import org.apache.http.*;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -53,6 +60,10 @@ public class Connection extends AsyncTask <String, Void, String> {
     private final String URL = "http://homepages.cs.ncl.ac.uk/2014-15/csc2022_team5/PHP/main.php";
     private String result;
     private String test;
+    HttpClient httpclient = new DefaultHttpClient();
+    CookieStore cookies = new BasicCookieStore();
+    HttpContext context = new BasicHttpContext();
+
     //private String key = "4E050FDDFB44E903225EC6C20C37752DB57B542E07D808248E5ABC720D8571E599A29295EB62230785369F5D9AA1E7D761656DA1918054E9E4B22970EBC59DE3";
 
     /* This is where the magic happens. This is what is run when the
@@ -79,6 +90,11 @@ public class Connection extends AsyncTask <String, Void, String> {
         try
         {
 
+            if(strings[1].equals("LOGIN"))
+            {
+                return this.loginConnect(nameValuePairs);
+            }
+
             return this.connect(nameValuePairs);
 
         }
@@ -93,8 +109,8 @@ public class Connection extends AsyncTask <String, Void, String> {
      * input from the server, turns it into a JSON object, and then sees if it can log in */
 
      public String connect(List<NameValuePair> nameValuePairs) throws IOException {
-        // Create a new HttpClient and Post Header
-        HttpClient httpclient = new DefaultHttpClient();
+
+         //context.setAttribute(ClientContext.COOKIE_STORE,cookies);
         HttpPost httppost = new HttpPost(URL);
 
         try {
@@ -105,7 +121,74 @@ public class Connection extends AsyncTask <String, Void, String> {
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
             /* Execute the post request and save the response */
-            HttpResponse response = httpclient.execute(httppost);
+            HttpResponse response = httpclient.execute(httppost, context);
+
+            Log.d("cookies",cookies.toString());
+
+            HttpEntity entity = response.getEntity();
+
+            /* Input stream for the result */
+            InputStream inputStream = entity.getContent();
+            /* Reads the data */
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+
+            /* Use a string builder to create a string of all the JSON stuff */
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+
+            String result = sb.toString();
+
+            Log.d("server", result);
+
+            /* check the value of status */
+            try {
+                JSONObject jObject = new JSONObject(result);
+
+
+                return jObject.toString();
+
+
+
+            }catch(Exception e)
+            {
+                throw new IOException("Error parsing JSON");
+            }
+
+        } catch (ClientProtocolException e) {
+            throw new IOException("Connection could not be established");
+
+        } catch (IOException e) {
+            throw new IOException("Connection could not be established");
+
+        }
+
+
+    }
+
+    public String loginConnect(List<NameValuePair> nameValuePairs) throws IOException {
+        // Create a new HttpClient and Post Header
+        httpclient = new DefaultHttpClient();
+         cookies = new BasicCookieStore();
+         context = new BasicHttpContext();
+        context.setAttribute(ClientContext.COOKIE_STORE,cookies);
+        HttpPost httppost = new HttpPost(URL);
+
+        try {
+            /* Creates name value pair to be sent via post to the server */
+//            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//            nameValuePairs.add(new BasicNameValuePair("username", username));
+//            nameValuePairs.add(new BasicNameValuePair("password", password));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            /* Execute the post request and save the response */
+            HttpResponse response = httpclient.execute(httppost, context);
+
+            Log.d("cookies",cookies.toString());
+
             HttpEntity entity = response.getEntity();
 
             /* Input stream for the result */
--- app/src/main/java/com/ncl/team5/lloydsmockup/Login.java
+++ app/src/main/java/com/ncl/team5/lloydsmockup/Login.java
@@ -26,6 +26,7 @@ public class Login extends Activity {
     private boolean netProbs = false;
     private boolean warning = false;
     private boolean locked = false;
+    public Connection connect;
 
 
     @Override
