05e136f1b21147a704cbf697674385f6ff5fb754
arclupin <a.tang2@ncl.ac.uk>
2015-03-06 12:08:04 +0000

    Revert "Commented out failing server connection for UI team"
    This reverts commit f94aa91be50161da5be4676de30005b5beca3e7b.

--- app/src/main/java/com/ncl/team5/lloydsmockup/Login.java
+++ app/src/main/java/com/ncl/team5/lloydsmockup/Login.java
@@ -145,26 +145,24 @@ public class Login extends Activity {
          */
 
 
-//        HTTPConnect test = new HTTPConnect();
-//
-//        try {
-//            return test.getLogin();
-//        }catch (Exception e)
-//        {
-//            netProbs = true;
-//            AlertDialog.Builder errorBox = new AlertDialog.Builder(this);
-//            errorBox.setMessage("Network Problems Detected")
-//                    .setCancelable(false)
-//                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                        public void onClick(DialogInterface dialog, int id) {
-//                            dialog.cancel();
-//                        }
-//                    });
-//            AlertDialog alert = errorBox.create();
-//            alert.show();
-//            return false;
-//        }
+        HTTPConnect test = new HTTPConnect();
 
-        return true;
+        try {
+            return test.getLogin();
+        }catch (Exception e)
+        {
+            netProbs = true;
+            AlertDialog.Builder errorBox = new AlertDialog.Builder(this);
+            errorBox.setMessage("Network Problems Detected")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = errorBox.create();
+            alert.show();
+            return false;
+        }
     }
 }
