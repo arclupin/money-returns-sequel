77d720d74107c869b252b8907e1c14a42dd30808
danhng <danhthanhnguyen1995@gmail.com>
2015-03-09 16:01:51 +0000

    Some minor updates on the UI for Payment Activity. - Cleaned the tabs. - Added the swipe views for pages.

---
+++ app/src/main/java/FragPager/Payment_FragmentPagerAdapter.java
@@ -0,0 +1,42 @@
+package FragPager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Thanh on 07-Mar-15.
+ */
+
+/*
+Adapter for pages to be displayed in the payments activity
+ */
+public class Payment_FragmentPagerAdapter extends FragmentPagerAdapter {
+
+    final static int FRAGMENT_VIEWS = 2;
+
+    public Payment_FragmentPagerAdapter(FragmentManager manager) { // not sure what this's for, probably needed for getting back a FragmentManager later on.
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int number) {
+ switch (number) {
+     case 0: {
+         return new FragmentOldAccount(); // existing recipient
+     }
+     case 1: {
+         return new FragmentNewAccount(); // new recipient
+     }
+     default : {
+         return null; //TODO
+     }
+ }
+    }
+
+    @Override
+    public int getCount() {
+        return FRAGMENT_VIEWS;
+    }
+
+}
