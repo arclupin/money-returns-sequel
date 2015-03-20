15f7f991a04b1e45b8df14bcf046d91a833e8fb7
danhng <danhthanhnguyen1995@gmail.com>
2015-03-20 01:02:32 +0000

    Set up the House share service initialisation and registration

--- app/src/main/java/HTTPConnect/Request_Params.java
+++ app/src/main/java/HTTPConnect/Request_Params.java
@@ -7,5 +7,6 @@ public class Request_Params {
     public static final String PARAM_TYPE = "TYPE";
     public static final String VAL_HS_INIT = "HS_INIT";
     public static final String VAL_HS_REGISTER = "HS_REG";
+    public static final String VAL_HS_CREATE_HOUSE = "HS_CREATE_HS";
     public static final String PARAM_USR = "USR";
 }
---
+++ app/src/main/java/HTTPConnect/Responses_Format.java
@@ -0,0 +1,13 @@
+package HTTPConnect;
+
+/**
+ * Created by Thanh on 16-Mar-15.
+ */
+/**
+ * Class storing the format of responses from the server
+ */
+public class Responses_Format {
+    public static final String RESPONSE_HOUSESHARE_NOT_JOINED = "0";
+    public static final String RESPONSE_HOUSESHARE_JOINED_SERVICE= "1";
+    public static final String RESPONSE_HOUSESHARE_JOINED_HOUSE = "2";
+}
